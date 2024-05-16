4b4d60a7bb056e7c93a8ba9a75769be2e6106c5f
zhangbing.xie <xiezhangbing@haylou.com>
2024-05-16 16:32:26 +0800

    add kotlin grammar file

---
+++ kotlin-test01/src/main/kotlin/com/study/kotlintest01/basic/FunctionDefinition.kt
@@ -0,0 +1,36 @@
+package com.study.kotlintest01.basic
+
+fun sum(a: Int, b: Int): Int {   // Int 参数，返回值 Int
+    return a + b
+}
+
+fun sum(a: Int, b: Int) = a + b
+
+public fun sum(a: Int, b: Int): Int = a + b   // public 方法则必须明确写出返回类型
+
+fun printSum(a: Int, b: Int): Unit {
+    print(a + b)
+}
+
+
+// 如果是返回 Unit类型，则可以省略(对于public方法也是这样)：
+public fun printSum(a: Int, b: Int) {
+    print(a + b)
+}
+fun vars(vararg v:Int){
+    for(vt in v){
+        print(vt)
+    }
+}
+
+// 测试
+fun main(args: Array<String>) {
+    vars(1,2,3,4,5)  // 输出12345
+}
+
+// 测试
+fun main(args: Array<String>) {
+    val sumLambda: (Int, Int) -> Int = {x,y -> x+y}
+    println(sumLambda(1,2))  // 输出 3
+}
+
